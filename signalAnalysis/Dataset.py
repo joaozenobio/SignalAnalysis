05207951f92ac549ebdc5db92290d90e852cbf82
joaozenobio <joao.zenobio@aluno.ufop.edu.br>
2022-07-15 17:44:29 -0300

    functional+images

--- signalAnalysis/Dataset.py
+++ signalAnalysis/Dataset.py
@@ -8,24 +8,23 @@ warnings.filterwarnings('ignore')
 
 class Dataset:
     def __init__(self, data_directory):
-        # Get csv and transform in pandas series
         signals = []
         for file in glob.glob(f"{data_directory}/*.csv"):
             signals.append(pd.read_csv(file, index_col=0).squeeze("columns"))
         signals.append(pd.read_csv("result_aJdKrfM.csv", index_col=0).squeeze("columns"))
 
-        data = pd.DataFrame()
-        j = 0
+        segmented_signals = []
         for signal in signals:
             for i in range(0, len(signal)-89, 10):
                 partition = signal[i:i+90].reset_index(drop=True)
-                data[j] = partition
-                j += 1
+                segmented_signals.append(partition)
+
+        data = pd.DataFrame(segmented_signals).reset_index(drop=True)
 
         self.original_data = data
 
-        data = data.diff().iloc[1:]
+        data = data.diff(axis=1).drop(columns=0)
 
-        data = minmax_scale(data).T
+        data = minmax_scale(data.values)
 
         self.data = data.reshape(data.shape[0], data.shape[1], 1)
--- signalAnalysis/main.py
+++ signalAnalysis/main.py
@@ -2,19 +2,21 @@ from scipy.cluster.hierarchy import linkage, dendrogram
 from FOSC import FOSC
 from Dataset import Dataset
 from Autoencoder import Autoencoder
+import hdbscan
+from sklearn.cluster import OPTICS
+from sklearn.cluster import DBSCAN
+from sklearn.manifold import TSNE
 
 from matplotlib.pyplot import cm
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import hdbscan
-from sklearn.cluster import OPTICS
-from sklearn.cluster import DBSCAN
 import sys
 from sklearn.metrics import mean_squared_error
-from sklearn.manifold import TSNE
 import plotly.express as px
 import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 sys.setrecursionlimit(10000)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -91,44 +93,79 @@ def plotDendrogram(Z, result, title, saveDescription=None):
 
 #### ############################################  Main script #########################################################
 
-listOfMClSize = [25, 50, 100, 200, 300]
-methodsLinkage = ["single", "average", "ward", "complete", "weighted"]
+os.makedirs("./results", exist_ok=True)
 
 print("\nPerforming experiments in dataset\n")
 
-data = Dataset("signals").data
+dataset = Dataset("signals")
 
-model = Autoencoder(data)
-# model.fit(data)
-# model.save("model2")
+model = Autoencoder(dataset.data)
+# # model.fit(dataset.data)
+# # model.save("model2")
 model.load("model")
-prediction = model.predict(data)
+prediction = model.predict(dataset.data)
 
 modProjecao = TSNE(n_components=2, learning_rate='auto', init='random')
-projecao2D = modProjecao.fit_transform(prediction)
-clustering = DBSCAN(eps=2).fit(projecao2D)
-projecao2D = pd.DataFrame(projecao2D)
-projecao2D["cluster"] = clustering.labels_
-projecao2D["label"] = list(range(prediction.shape[0]))
-fig = px.scatter(projecao2D, x=0, y=1, color="cluster", labels="label", hover_data=["label"])
-fig.show()
+TSNE_2D = modProjecao.fit_transform(prediction)
+clustering = DBSCAN(eps=2).fit(TSNE_2D)
+TSNE_2D = pd.DataFrame(TSNE_2D)
+TSNE_2D["cluster"] = clustering.labels_
+TSNE_2D["label"] = list(range(prediction.shape[0]))
+fig = px.scatter(TSNE_2D, x=0, y=1, color="cluster", labels="label", hover_data=["label"])
+fig.write_image(f"./results/TSNE_2D.jpeg")
 
-method = OPTICS().fit(prediction)
-print(method.labels_)
+optics_results = OPTICS(min_samples=15).fit(prediction)
+os.makedirs("./results/OPTICS", exist_ok=True)
+print(f"Number of clusters in OPTICS results = {len(set(optics_results.labels_))}")
+for cluster_number in set(optics_results.labels_):
+    print(f"Saving OPTICS clustering results for cluster {cluster_number}")
+    index_list = [i for i, item in enumerate(optics_results.labels_) if item == cluster_number]
+    print(f"Number of signals in cluser {cluster_number} = {len(index_list)}")
+    signal_list = dataset.original_data.iloc[index_list]
+    fig = make_subplots(rows=(len(signal_list.values) // 3) + 1, cols=3)
+    i = 0
+    for signal in signal_list.values:
+        fig.add_trace(
+            go.Scatter(x=list(range(len(signal))), y=signal),
+            row=((i // 3) + 1), col=((i % 3) + 1)
+        )
+        i += 1
+    fig.update_layout(height=(((len(signal_list) // 3) + 1) * 250) + 250, title_text="Cluster " + str(cluster_number))
+    fig.write_image(f"./results/OPTICS/cluster_{cluster_number}.jpeg")
 
-method = hdbscan.HDBSCAN()
-method.fit(prediction)
-print(method.labels_)
 
-# Running tests
+hdbscan_results = hdbscan.HDBSCAN(min_cluster_size=15)
+hdbscan_results.fit(prediction)
+os.makedirs("./results/HDBSCAN", exist_ok=True)
+print(f"Number of clusters in HDBSCAN results = {len(set(optics_results.labels_))}")
+for cluster_number in set(hdbscan_results.labels_):
+    print(f"Saving HDBSCAN clustering results for cluster {cluster_number}")
+    index_list = [i for i, item in enumerate(hdbscan_results.labels_) if item == cluster_number]
+    print(f"Number of signals in cluser {cluster_number} = {len(index_list)}")
+    signal_list = dataset.original_data.iloc[index_list]
+    fig = make_subplots(rows=(len(signal_list.values) // 3) + 1, cols=3)
+    i = 0
+    for signal in signal_list.values:
+        fig.add_trace(
+            go.Scatter(x=list(range(len(signal))), y=signal),
+            row=((i // 3) + 1), col=((i % 3) + 1)
+        )
+        i += 1
+    fig.update_layout(height=(((len(signal_list) // 3) + 1) * 250) + 250, title_text="Cluster " + str(cluster_number))
+    fig.write_image(f"./results/HDBSCAN/cluster_{cluster_number}.jpeg")
+
+listOfMClSize = [25, 50, 100, 200, 300]
+methodsLinkage = ["single", "average", "ward", "complete", "weighted"]
+
+os.makedirs("./results/FOSC", exist_ok=True)
 for m in listOfMClSize:
     print("--------------------------------------- MCLSIZE = %d ---------------------------------------" % m)
 
     for lm in methodsLinkage:
 
         titlePlot = lm + " and mClSize = " + str(m)
-        savePath = "output/" + lm + "-" + str(m) + ".png"
-        saveDendrogram = "dendrograms/dendrogram-" + lm + "-mClSize-" + str(m) + ".png"
+        savePath = "./results/FOSC/output/" + lm + "-" + str(m) + ".png"
+        saveDendrogram = "./results/FOSC/dendrogram-" + lm + "-mClSize-" + str(m) + ".png"
 
         print("Using linkage method %s" % lm)
         Z = linkage(prediction, method=lm, metric="euclidean")
