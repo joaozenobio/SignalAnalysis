21a1e9252f1f3d617b95fa89dcfa27f1acf7f99a
joaozenobio <joao.zenobio@aluno.ufop.edu.br>
2022-08-18 23:33:13 -0300

    todo

--- signalAnalysis/main.py
+++ signalAnalysis/main.py
@@ -9,14 +9,18 @@ import os
 import sys
 import plotly.express as px
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+import seaborn as sns
 
 # TODO:
 #  Report
 #  for + Reachability plot OPTICS
 #  for + Dendrogram HDBSCAN
 #  for + Dendrogram FOSC
-#  Print example "result_..." in TSNE 2D
 #  Calculate metrics
+#  Print example "result_..." in TSNE 2D with best metrics
 #  Train YOLO with new images
 
 sys.setrecursionlimit(10000)
@@ -32,30 +36,37 @@ model = Autoencoder(dataset.data)
 model.load("model2")
 prediction = model.predict(dataset.data)
 
-modProjecao = TSNE(n_components=2, learning_rate='auto', init='random')
-TSNE_2D = modProjecao.fit_transform(prediction)
+tsne_results = TSNE(n_components=2, learning_rate='auto', init='random')
+TSNE_2D = tsne_results.fit_transform(prediction)
 TSNE_2D = pd.DataFrame(TSNE_2D)
 TSNE_2D["label"] = list(range(prediction.shape[0]))
 
-optics_results = OPTICS(min_samples=5, min_cluster_size=5).fit(prediction)
-TSNE_2D["OPTICS"] = optics_results.labels_
-
-hdbscan_results = hdbscan.HDBSCAN(min_samples=5, min_cluster_size=5)
-hdbscan_results.fit(prediction)
-TSNE_2D["HDBSCAN"] = hdbscan_results.labels_
-
-listOfMClSize = [25, 50, 100, 200, 300]
-methodsLinkage = ["single", "average", "ward", "complete", "weighted"]
+listOfMClSize = [5, 10, 15, 20, 25, 30]
+for m in listOfMClSize:
+    hdbscan_results = hdbscan.HDBSCAN(min_cluster_size=m)
+    hdbscan_results.fit(prediction)
+    fig, ax = plt.subplots()
+    fig.set_size_inches(15, 15)
+    hdbscan_results.condensed_tree_.plot()
+    plt.savefig(f'HDBSCAN_{m}.png', dpi=100)
 
 for m in listOfMClSize:
-    for lm in methodsLinkage:
-        titlePlot = lm + " and mClSize = " + str(m)
-        Z = linkage(prediction, method=lm, metric="euclidean")
-        foscFramework = FOSC(Z, mClSize=m)
-        infiniteStability = foscFramework.propagateTree()
-        partition = foscFramework.findProminentClusters(1, infiniteStability)
-        TSNE_2D[f"FOSC_{m}_{lm}"] = partition
+    optics_results = OPTICS(min_cluster_size=m).fit(prediction)
+    reachability = optics_results.reachability_[optics_results.ordering_]
+    fig, ax = plt.subplots()
+    fig.set_size_inches(15, 15)
+    ax.plot(reachability)
+    plt.savefig(f'OPTICS_{m}.png', dpi=100)
 
-for m in listOfMClSize:
-    for lm in methodsLinkage:
-        pass
+# methodsLinkage = ["single", "average", "ward", "complete", "weighted"]
+#
+# for m in listOfMClSize:
+#     for lm in methodsLinkage:
+#         titlePlot = lm + " and mClSize = " + str(m)
+#         Z = linkage(prediction, method=lm, metric="euclidean")
+#         foscFramework = FOSC(Z, mClSize=m)
+#         infiniteStability = foscFramework.propagateTree()
+#         partition = foscFramework.findProminentClusters(1, infiniteStability)
+#         TSNE_2D[f"FOSC_{m}_{lm}"] = partition
+#
+
